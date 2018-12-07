add3165d40af688079fa5411bdde25ef9221d57d
CasperCarton <casper.carton@gmail.com>
2018-12-07 17:00:09 +0100

    blokje dobbelsteen

---
+++ public/javascripts/testdice.js
@@ -0,0 +1,32 @@
+document.getElementById("dice").onclick = function() { diceroll() };
+
+function diceroll(){
+        
+            var number = Math.floor(Math.random()*6) + 1;
+            var dicenumber = document.getElementById("dice");
+            
+            if(number == 1){
+                dicenumber.innerHTML = "<img src=images/dice1.png>";
+            }
+
+            if(number == 2){
+                dicenumber.innerHTML = "<img src=images/dice2.png>";
+            }
+
+            if(number == 3){
+                dicenumber.innerHTML = "<img src=images/dice3.png>";
+            }
+
+            if(number == 4){
+                dicenumber.innerHTML = "<img src=images/dice4.png>";
+            }
+
+            if(number == 5){
+                dicenumber.innerHTML = "<img src=images/dice5.png>";
+            }
+
+            if(number == 6){
+                dicenumber.innerHTML = "<img src=images/dice6.png>";
+            }
+            
+        }
