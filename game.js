0069cf64030883cb5ba4c078b1dda5242cd5a3f1
BonnoNieuwenhout <bonno.nieuwenhout@gmail.com>
2018-12-10 15:59:17 +0100

    fix

--- game.js
+++ game.js
@@ -44,9 +44,9 @@ game.prototype.transitionMatrix = [
     [0, 0, 0, 0, 0, 0, 0]    //ABORTED
 ];
 
-game.prototype.isValidTransiction = function (from, to){
-    //console.assert(typeof from == "string", "%s: Expecting a string, got a %s", arguments.callee.name, typeof from);
-    //console.assert(typeof to == "string", "%s: Expecting a string, got a %s", arguments.callee.name, typeof to);
+game.prototype.isValidTransition = function (from, to){
+    console.assert(typeof from == "string", "%s: Expecting a string, got a %s", arguments.callee.name, typeof from);
+    console.assert(typeof to == "string", "%s: Expecting a string, got a %s", arguments.callee.name, typeof to);
     console.assert( from in game.prototype.transitionStates == true, "%s: Expecting %s to be a valid transition state", arguments.callee.name, from);
     console.assert( to in game.prototype.transitionStates == true, "%s: Expecting %s to be a valid transition state", arguments.callee.name, to);
 
