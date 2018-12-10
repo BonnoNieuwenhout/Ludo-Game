c0deb18db88439d505ff2a3f57ec65ff7cf9629c
BonnoNieuwenhout <bonno.nieuwenhout@gmail.com>
2018-12-10 12:18:12 +0100

    Start with the dice object

--- public/javascripts/Pion.js
+++ public/javascripts/Pion.js
@@ -2,8 +2,14 @@ document.getElementById("Pion").onclick = function() { movePion() };
 
 function movePion(){
     var Pion = document.getElementById("Pion");
-
+    var Position = document.getElementById("Position");
+    var PositionId = document.getElementById("Position").id;
+    var Movement = document.getElementById(PositionId + number);
+    //According to the rolled number the Pion should move to 
+    //(The current Position number + the rolled dice number) id
     if(number == 1){
-        Pion
+        if(Position.innerHTML = Pion){
+            Movement.innerHTML = Pion;
+        } 
     }
 }
