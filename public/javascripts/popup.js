7ee9346561d9e3c75b70adbf1027d7107a51c9ed
BonnoNieuwenhout <bonno.nieuwenhout@gmail.com>
2018-12-10 14:16:27 +0100

    Trying to make it work

--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -1,6 +1,11 @@
-document.getElementById("myPopup").onclick = function() { popFunction() }
+document.getElementById("two").onclick = function() { popFunction() }
 
 function popFunction(){
     var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+    
+    if(popup.style.display === "none"){
+        popup.style.display = "block";
+    } else{
+        popup.style.display = "none"
+    }
 }
