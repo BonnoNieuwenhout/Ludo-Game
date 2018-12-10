c2d242d85218b424a4a4f1a3bc49c2bb5ec23af9
BonnoNieuwenhout <bonno.nieuwenhout@gmail.com>
2018-12-10 15:53:22 +0100

    Made the whole game

--- game.js
+++ game.js
@@ -29,3 +29,103 @@ game.prototype.transitionStates["D"] = 12; //D won
 game.prototype.transitionStates["ABORTED"] = 13;
 
 //What to do now?
+
+game.prototype.transitionMatrix = [
+    [0, 1, 0, 0, 0, 0, 0],   //0 JOINT
+    [1, 0, 1, 0, 0, 0, 0],   //1 JOINT
+    [1, 1, 1, 0, 0, 0, 0],   //2 JOINT
+    [1, 1, 1, 1, 0, 0, 0],   //3 JOINT
+    [0, 0, 0, 1, 0, 0, 1],   //4 JOINT (note: once we have two players, there is no way back!)
+    [0, 0, 0, 1, 1, 1, 1],   //Pion Moved
+    [0, 0, 0, 0, 0, 0, 0],   //A WON
+    [0, 0, 0, 0, 0, 0, 0],   //B WON
+    [0, 0, 0, 0, 0, 0, 0],   //C WON
+    [0, 0, 0, 0, 0, 0, 0],   //D WON
+    [0, 0, 0, 0, 0, 0, 0]    //ABORTED
+];
+
+game.prototype.isValidTransiction = function (from, to){
+    //console.assert(typeof from == "string", "%s: Expecting a string, got a %s", arguments.callee.name, typeof from);
+    //console.assert(typeof to == "string", "%s: Expecting a string, got a %s", arguments.callee.name, typeof to);
+    console.assert( from in game.prototype.transitionStates == true, "%s: Expecting %s to be a valid transition state", arguments.callee.name, from);
+    console.assert( to in game.prototype.transitionStates == true, "%s: Expecting %s to be a valid transition state", arguments.callee.name, to);
+
+    let i, j;
+
+    if(! (from in game.prototype.transitionStates)){
+        return false;
+    }
+    else{
+        i = game.prototype.transitionStates[from];
+    }
+
+    if(! (to in game.prototype.transitionStates)){
+        return false;
+    }
+    else{
+        j = game.prototype.transitionStates[to];
+    }
+
+    return (game.transitionMatrix[i][j] > 0);
+};
+
+game.prototype.isValidState = function (state){
+    return(state in game.prototype.transitionStates);
+};
+
+game.prototype.setStatus = function (w){
+    console.assert(typeof w == "string", "%s: Expecting a string, got a %s", arguments.callee.name, typeof w);
+
+    if (game.prototype.isValidState(w) && game.prototype.isValidTransition(this.gameState, w)) {
+        this.gameState = w;
+        console.log("[STATUS] %s", this.gameState);
+    }
+    else {
+        return new Error("Impossible status change from %s to %s", this.gameState, w);
+    }
+};
+
+game.prototype.hasFourConnectedPlayers = function(){
+    return (this.gameState == "4 JOINT");
+};
+
+game.prototype.addPlayer = function (player) {
+
+    console.assert(player instanceof Object, "%s: Expecting an object (WebSocket), got a %s", arguments.callee.name, typeof player);
+
+    if (this.gameState != "0 JOINT" && this.gameState != "1 JOINT" && this.gameState != "2 JOINT" && this.gameState != "3 JOINT") {
+        return new Error("Invalid call to addPlayer, current state is %s", this.gameState);
+    }
+
+    /*
+     * revise the game state??
+     */ 
+    var error = this.setStatus("3 JOINT");
+    if(error instanceof Error){
+        this.setStatus("4 JOINT");
+    }
+
+    if (this.playerA == null) {
+        this.playerA = player;
+        return "A";
+    }
+    else {
+        if(this.playerB == null){
+        this.playerB = player;
+        return "B";
+        }
+
+        if(this.playerC == null){
+            this.playerC = player;
+            return "B";
+        }
+
+        if(this.playerD == null){
+        this.playerD = player;
+        return "B";
+        }
+    }
+};
+
+module.exports = game;
+
--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -2,7 +2,7 @@ document.getElementById("two").onclick = function() { popFunction() }
 
 function popFunction(){
     var popup = document.getElementById("myPopup");
-    
+    console.log(popup);
     if(popup.style.display === "none"){
         popup.style.display = "block";
     } else{
