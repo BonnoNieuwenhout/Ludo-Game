e95e0471516dc2f25b05b55f659a8885837578b7
CasperCarton <casper.carton@gmail.com>
2018-12-11 16:12:16 +0100

    clock werkt nu

--- public/javascripts/clock.js
+++ public/javascripts/clock.js
@@ -15,7 +15,7 @@ function add(){
         }
     }
 
-    h2.textContent = (hours ? (hours>9 ? hours : "0" + hours) : "00")
+    clock.textContent = (hours ? (hours>9 ? hours : "0" + hours) : "00")
     + ":" + (minutes ? (minutes>9 ? minutes : "0" + minutes) : "00") + ":" +
     (seconds>9 ? seconds : "0" + seconds);
 
@@ -26,7 +26,7 @@ function timer(){
 }
 timer();
 
-start.onclick = alert("doe het!");
+start.onclick = timer;
 
 stop.onclick = function(){
     clearTimeout(t);
